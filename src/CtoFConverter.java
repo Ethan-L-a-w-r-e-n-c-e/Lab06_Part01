5768df17058c64890047075e0273a1f42d3a172d
Ethan-L-a-w-r-e-n-c-e <lawrence.614730@swl.k12.oh.us>
2024-10-02 08:21:29 -0400

    e

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,5 +1,27 @@
-public class Main {
+import java.util.Scanner;
+
+public class CtoFConverter {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        double celsius = 0;
+        double fahrenheit = 0;
+        boolean valid = false;
+
+        do {
+            System.out.println("Please enter a temperature in degrees C to be converted to degrees F");
+            if(scan.hasNextDouble()){
+                celsius = scan.nextDouble();
+                fahrenheit = ((1.8*celsius)+32);
+                valid = true;
+            } else {
+                scan.nextLine();
+                System.out.println("You have entered in the wrong data type");
+                System.out.println();
+            }
+
+        } while (!valid);
+        System.out.printf("Degrees C: %8.2f",celsius);
+        System.out.printf("\nDegrees F: %8.2f",fahrenheit);
     }
 }
